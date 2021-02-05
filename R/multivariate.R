b8406979ce94f18bc368abc343f3613c3bb5aabd
dicook <visnut@gmail.com>
2021-02-05 17:39:00 +1100

    fillingin more functions

--- R/multivariate.R
+++ R/multivariate.R
@@ -14,7 +14,7 @@
 #' @examples
 #' require(ggplot2)
 #' require(tibble)
-#' ell2d <- f_vc_ellipse(vc = matrix(c(4, 2, 2, 6), ncol=2, byrow=T), xm = c(1,1)
+#' ell2d <- f_vc_ellipse(vc = matrix(c(4, 2, 2, 6), ncol=2, byrow=T), xm = c(1,1))
 #' ell2d <- as_tibble(ell2d)
 #' ggplot(ell2d, aes(x = V1, y = V2)) + geom_point()
 f_vc_ellipse <- function(vc, xm, n = 500) {
@@ -27,3 +27,42 @@ f_vc_ellipse <- function(vc, xm, n = 500) {
 
 	x <- x + matrix(rep(xm, each = n), ncol = p)
 }
+
+#' Normalise a vector to have length 1
+f_norm_vec<-function(x) {
+	x <- x/f_norm(x)
+	x
+}
+
+#' Calculate the norm of a vector
+f_norm <- function(x) { sqrt(sum(x^2)) }
+
+#' Generate a sample from a multivariate normal
+#'
+#' This function generates a  sample of size n from a
+#' multivariate normal distribution
+#'
+#' @param n number of points to generate
+#' @param p dimension
+#' @param mn mean of the distribution, a vector of length
+#' equal to the dimension of vc
+#' @param vc symmetric square matrix describing the
+#' variance-covariance matrix which defines the shape
+#' of the ellipse.
+#' @return matrix of size n x p
+#' @export
+#' @examples
+#' require(ggplot2)
+#' require(tibble)
+#' d <- f_gen_mvn(n=100, p=2, mn = c(1,1), vc = matrix(c(4, 2, 2, 6), ncol=2, byrow=T))
+#' d <- as_tibble(d)
+#' ggplot(d, aes(x = V1, y = V2)) + geom_point()
+f_gen_mvn<-function(n=100, p=5, mn=rep(0,p), vc=diag(rep(1,p))) {
+	x <-  matrix(rnorm(n*p), ncol=p)
+	ev <- eigen(vc)
+	vcsqrt <- diag(sqrt(ev$values))%*%t(ev$vectors)
+	x <- x%*%vcsqrt
+	x <- x + matrix(rep(mn,n), ncol=p,  byrow=T)
+	return(x)
+}
+
