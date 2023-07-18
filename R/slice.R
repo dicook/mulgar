c38f9668058d4176caa6e105902691ee36e80ab6
Ursula Laa <ursula.laa@boku.ac.at>
2023-07-18 11:13:39 +0200

    Finishing slice display documentation

--- R/slice.R
+++ R/slice.R
@@ -1,6 +1,10 @@
-#' This function generates an axis-parallel slice display
+#' Generate an axis-parallel slice display
 #'
-#' more info on the function
+#' Following the slice definition available in `tourr`
+#' this function returns a `ggplot2` display of a slice
+#' defined via the projection onto two of the variables.
+#' Note that because the underlying function works with any
+#' projection, the axis labels need to be set by the user.
 #'
 #' @param data data frame containing only variables used for the display
 #' @param h slice thickness
@@ -10,20 +14,36 @@
 #'    if NULL the mean of the data is used
 #' @param col grouping vector mapped to color in the display
 #'
-#' @return ggplot object showing the sliced data
+#' @return ggplot2 object showing the sliced data
 #' @export
+#' @seealso ggslice_projection
 #' @examples
 #' d <- geozoo::sphere.hollow(4, 1000)$points
 #' ggslice(d, 0.3, 1, 2)
 #' ggslice(d, 0.3, 1, 2, center = c(0, 0, 0.7, 0))
-
-
 ggslice <- function(data, h, v1=1, v2=2, center=NULL,
 										col=NULL) {
 	plane <- basis_proj(v1, v2, ncol(data))
 	ggslice_projection(data, h, plane, center, col)
 }
 
+#' Generate slice display
+#'
+#' @param data data frame containing only variables used for the display
+#' @param h slice thickness
+#' @param proj projection matrix from p to 2 dimensions
+#' @param center center point vector used for anchoring the slice,
+#'    if NULL the mean of the data is used
+#' @param col grouping vector mapped to color in the display
+#'
+#' @return ggplot2 object showing the sliced data
+#' @export
+#' @seealso ggslice
+#' @examples
+#' d <- geozoo::sphere.hollow(4, 1000)$points
+#' ggslice_projection(d, 0.3, tourr::basis_random(4))
+#' ggslice_projection(d, 0.3, tourr::basis_random(4),
+#'                    center = c(0.4, 0.4, 0.4, 0.4))
 ggslice_projection <- function(data, h, proj, center=NULL,
 															col=NULL) {
 	d <- tourr::anchored_orthogonal_distance(proj, data, anchor = center)
@@ -38,11 +58,18 @@ ggslice_projection <- function(data, h, proj, center=NULL,
 		coord_fixed()
 }
 
+#' Generate basis vector
+#'
+#' @noRd
 basis_vector <- function(i, p){
 	v <- rep(0, p)
 	v[i] <- 1
 	v
 }
+
+#' Generate axis-parallel projection matrix
+#'
+#' @noRd
 basis_proj <- function(i, j, p){
-	matrix(c(basisVector(i, p), basisVector(j, p)), ncol=2)
+	matrix(c(basis_vector(i, p), basis_vector(j, p)), ncol=2)
 }
